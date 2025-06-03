4ebf5ef4edbfd7d056233b6c6cddac3c83c582d6
Monique Wakefield <monique_wakefield@yahoo.com>
2025-06-03 14:48:51 -0500

    Update subscribers.js

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -43,6 +43,19 @@ router.delete('/:id', (req, res) => {
 
 })
 
+function getSubscriber(req, res, next) {
+    let subscriber;
+    try {
+        subscriber = wait Subscriber.findById(req.params.id);
+        if (subscriber == null) {
+            return res.status(404).json({ message: 'Cannot find subscriber' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+    res.subscriber = subscriber;
+    next();
+}
 module.exports = router;
 // Export the router to be used in server.js
 
