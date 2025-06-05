29da6ffe7829b67f5a6cfc2dc8220b20dec46c92
Monique Wakefield <monique_wakefield@yahoo.com>
2025-06-05 07:47:46 -0500

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
+
 
 router.get('/', (req, res) => {
   res.send('Subscribers route is alive');
 });
+
+router.post('/', (req, res) => {
+  const { email } = req.body;
+
+  if (!email || !email.includes('@')) {
+    return res.status(400).json({ message: 'Invalid email address' });
+  }
+
+  userController.addSubscriber(req, res);
+  // Here you would typically save the subscriber to a database
+  console.log(`New subscriber added: ${email}`);
+  
+  res.status(201).json({ message: 'Subscription successful', email });
+}
+router.get('/:id', userController.getSubscriberById)
+);
 
 module.exports = router;
